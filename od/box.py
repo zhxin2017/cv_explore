fix training bugs like unable to backprop due to inplace op

od/box.py
@@ -11,13 +11,34 @@ def pad_bbox(bboxes, indices, n_query):
     return bboxes_padded, indices_padded
 
 
-def resize_bbox(b_xyxy_l, ratio):
-    return [[i * ratio for i in b] for b in b_xyxy_l]
+def offset_box(boxes_xywh, offset_h, offset_w):
+    print(boxes_xywh.shape)
+    boxes_xywh[:, :1] = boxes_xywh[:, :1] + offset_w
+    boxes_xywh[:, 1:2] = boxes_xywh[:, 1:2] + offset_h
+    return boxes_xywh
 
 
-def xywh_to_xyxy(bboxes, offset_h, offset_w):
-    bboxes[:, :1] = bboxes[:, :1] + offset_w
-    bboxes[:, 1:2] = bboxes[:, 1:2] + offset_h
-    bboxes[:, 2:3] = bboxes[:, 2:3] + bboxes[:, :1]
-    bboxes[:, 3:4] = bboxes[:, 3:4] + bboxes[:, 1:2]
-    return bboxes
+def xywh_to_xyxy(boxes):
+    boxes[:, 2:3] = boxes[:, 2:3] + boxes[:, :1]
+    boxes[:, 3:4] = boxes[:, 3:4] + boxes[:, 1:2]
+    return boxes
+
+
+def cxcywh_to_xyxy(boxes):
+    x_c, y_c, w, h = boxes.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=-1)
+
+# def cxcywh_to_xyxy2(boxes):
+#     boxes2 = boxes + 0
+#     b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+#          (x_c + 0.5 * w), (y_c + 0.5 * h)]
+#     return torch.stack(b, dim=-1)
+
+
+def xyxy_to_cxcywh(boxes):
+    x0, y0, x1, y1 = boxes.unbind(-1)
+    b = [(x0 + x1) / 2, (y0 + y1) / 2,
+         (x1 - x0), (y1 - y0)]
+    return torch.stack(b, dim=-1)

od/detr_dataset.py
@@ -8,7 +8,7 @@ from od.config_file import train_img_dir, val_img_dir
 
 
 class OdDataset(Dataset):
-    def __init__(self, img_dict, train=True, sample_num=None, resize=(192, 256), n_query=300, random_shift=True):
+    def __init__(self, img_dict, train=True, sample_num=None, resize=(384, 512), n_query=300, random_shift=True):
         self.sample_num = sample_num
         self.resize = resize
         self.random_shift = random_shift
@@ -21,6 +21,8 @@ class OdDataset(Dataset):
         for iid, img_info in self.img_dict.items():
             iscrowd = False
             objs = img_info['objs']
+            if len(objs) == 0:
+                continue
             for obj_ in objs:
                 if obj_['iscrowd'] == 1:
                     iscrowd = True
@@ -43,17 +45,18 @@ class OdDataset(Dataset):
         out_ratio = self.resize[0] / self.resize[1]
         img, offset_h, offset_w = image.pad_img(img, random_offset=self.random_shift, out_ratio=out_ratio)
 
-        bboxes, cids = [], []
+        boxes, cids = [], []
         for o in objs:
-            bboxes.append(o['bbox'])
+            boxes.append(o['bbox'])
             cids.append(o['category_id'])
 
-        bboxes = torch.tensor(bboxes)
+        boxes = torch.tensor(boxes)
 
         resize_factor = self.resize[0] / img.shape[0]
-        bboxes = box.xywh_to_xyxy(bboxes, offset_h, offset_w) * resize_factor
+        boxes = box.offset_box(boxes, offset_h, offset_w)
+        boxes = box.xywh_to_xyxy(boxes) * resize_factor
 
-        bboxes_padded, indices_padded = box.pad_bbox(bboxes, cids, self.n_query)
+        bboxes_padded, indices_padded = box.pad_bbox(boxes, cids, self.n_query)
 
         img = torch.permute(img, [2, 0, 1])
 

od/detr_model.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 from model import base
 import pe
+from config_category import category_num
 
 
 def attention(q, k, v):
@@ -86,7 +87,7 @@ class DecoderLayer(nn.Module):
 
 
 class DETR(nn.Module):
-    def __init__(self, d_cont, n_head=8, n_enc_layer=6, n_dec_layer=6, n_query=300, n_category=80, device=torch.device('mps')):
+    def __init__(self, d_cont, n_head=8, n_enc_layer=6, n_dec_layer=6, n_query=300, device=torch.device('mps')):
         super().__init__()
         self.d_cont = d_cont
         self.d_pos_emb = d_cont // 2
@@ -110,7 +111,7 @@ class DETR(nn.Module):
         self.hw_delta_mlp = base.MLP(d_cont, 256, 2, 2)
         self.src_hw_mlp = base.MLP(d_cont, 256, self.d_pos_emb, 2)
         self.anchor_emb = pe.Embedding1D(n_query, 4, device)
-        self.classify_mlp = base.MLP(d_cont, 256, n_category, 2)
+        self.classify_mlp = base.MLP(d_cont, 256, category_num, 2)
 
         decoder_layers = []
         for i in range(n_dec_layer):
@@ -142,6 +143,9 @@ class DETR(nn.Module):
 
         anchors = self.anchor_emb(B)
 
+        xy = anchors[..., :2] + 0
+        wh = anchors[..., 2:] + 0
+
         q_tgt_pos = self.pe_proj(anchors[..., :2])
         q_tgt_hw = self.hw_proj(anchors[..., 2:])
         q_tgt_cont = torch.zeros(B, self.n_query, self.d_cont, device=self.device)
@@ -153,15 +157,16 @@ class DETR(nn.Module):
             q_tgt_cont = dec_layer(q_tgt_cont, q_tgt_pos, q_tgt_hw, k_src_pos, k_src_hw, v_src_cont)
             tgt_pos_delta = self.pos_delta_mlp(q_tgt_cont)
             tgt_hw_delta = self.hw_delta_mlp(q_tgt_cont)
-            anchors[..., :2] = anchors[..., :2] + tgt_pos_delta
-            anchors[..., 2:] = anchors[..., 2:] + tgt_hw_delta
+            xy = xy + tgt_pos_delta
+            wh = wh + tgt_hw_delta
+
             if i < self.n_dec_layer - 1:
                 q_tgt_pos = self.pe_proj(anchors[..., :2])
                 q_tgt_hw = self.hw_proj(anchors[..., 2:])
 
-        categories = self.classify_mlp(q_tgt_cont)
-
-        return categories, anchors
+        cls_logits = self.classify_mlp(q_tgt_cont)
+        boxes = torch.concat((xy, wh), dim=-1)
+        return boxes, cls_logits
 
 
 if __name__ == '__main__':

od/pe.py
@@ -52,11 +52,11 @@ class Embedding1D(nn.Module):
         return emb.unsqueeze(0).repeat(B, 1, 1)
 
 
-def gen_pos_2d(x, device=torch.device('mps')):
+def gen_pos_2d(x, device=torch.device('mps'), feature_map_size=64):
     B, H, W, C = x.shape
     row_indices = torch.arange(H, device=device).unsqueeze(1).repeat(1, W).unsqueeze(-1)
     col_indices = torch.arange(W, device=device).unsqueeze(0).repeat(H, 1).unsqueeze(-1)
-    positions = torch.concat((row_indices / H, col_indices / W), dim=-1) * 0.5
+    positions = torch.concat((row_indices / feature_map_size, col_indices / feature_map_size), dim=-1) * 0.5
     positions = positions.unsqueeze(0).repeat(B, 1, 1, 1)
     return positions
 