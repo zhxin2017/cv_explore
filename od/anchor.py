130670cf1f18672e8e06071d24bbb26fef265cac
zhang xin <zhxin1994@gmail.com>
2024-04-04 14:26:35 +0800

    make anchors less dense

--- od/anchor.py
+++ od/anchor.py
@@ -1,31 +1,31 @@
-from od.config import anchor_min_size, anchor_max_size
-import torch
+import math
+
+h_iter = [1, 2, 4, 6, 8]
+w_iter = [1, 2, 4, 6, 8]
+anchor_max_size_W = w_iter[-1]
+anchor_max_size_H = h_iter[-1]
+anchor_max_size = max(anchor_max_size_H, anchor_max_size_W)
 
 
-def generate_anchors(variable_stride=False):
-    anchor_max_size_W = anchor_max_size
-    anchor_max_size_H = anchor_max_size
-    ratio_thresh = 5
-
+def generate_anchors(stride_ratio=-1.0):
+    ratios = [1, 2, 3, 4]
     anchors = []
-    for h in range(anchor_min_size, anchor_max_size_H + 1, anchor_min_size):
-        if variable_stride:
-            y_stride = int((h / anchor_min_size) ** 0.5 * anchor_min_size)
-        else:
-            y_stride = anchor_min_size
-        for w in range(anchor_min_size, anchor_max_size_W + 1, anchor_min_size):
-            x_stride = int((w / anchor_min_size) ** 0.5 * anchor_min_size)
-            ratio = max(w, h) / min(w, h)
+    for h in h_iter:
+        y_stride = 1 if stride_ratio == -1 else math.ceil(h * stride_ratio)
+        for w in w_iter:
+            x_stride = 1 if stride_ratio == -1 else math.ceil(w * stride_ratio)
+
+            ratio = max(w, h) // min(w, h)
             # print(ratio)
-            if ratio > ratio_thresh:
+            if ratio not in ratios:
                 continue
             for y1 in range(0, anchor_max_size_H, y_stride):
                 y2 = y1 + h
-                if y2 > anchor_max_size_H - anchor_min_size / 2:
+                if y2 > anchor_max_size_H - .5 * y_stride:
                     continue
                 for x1 in range(0, anchor_max_size_W, x_stride):
                     x2 = x1 + w
-                    if x2 > anchor_max_size_W - anchor_min_size / 2:
+                    if x2 > anchor_max_size_W - .5 * x_stride:
                         continue
                     anchors.append([x1 / anchor_max_size_W, y1 / anchor_max_size_H, x2 / anchor_max_size_W,
                                     y2 / anchor_max_size_H])
@@ -33,22 +33,21 @@ def generate_anchors(variable_stride=False):
 
 
 if __name__ == '__main__':
-    anchors = generate_anchors(variable_stride=True)
+    anchors = generate_anchors(stride_ratio=1/3)
     print(len(anchors))
     # print(anchors)
 
     import torch
-    import matplotlib
     import random
     from matplotlib import pyplot as plt
 
     colors = ['gray', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'blue', 'olive', 'cyan']
 
     img = torch.ones([128, 128, 3]) * 0.6
-    fig, axes = plt.subplots(4, 4)
+    fig, axes = plt.subplots(8, 8)
     random.shuffle(anchors)
-    for i in range(4):
-        for j in range(4):
+    for i in range(8):
+        for j in range(8):
             axes[i, j].axis('off')
             axes[i, j].imshow(img)
 
