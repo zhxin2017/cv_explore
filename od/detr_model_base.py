e2920014cab97258195d820f9a1be1a7c12e7260
zhang xin <zhxin1994@gmail.com>
2024-04-08 22:22:02 +0800

    add base/original detr model. anchor-based didn't workout, or maybe more computing power is needed to converge

---
+++ od/detr_model_base.py
@@ -0,0 +1,76 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from model import base, pe, tsfm, enc
+from od.config import n_cls
+from common.config import img_size
+
+
+class DetrDecoder(nn.Module):
+    def __init__(self, d_cont, d_enc_coord_emb, d_head, n_dec_layer, n_query):
+        super().__init__()
+
+        dq_dec = dk_dec = dv_dec = d_cont + d_enc_coord_emb * 2
+        self.n_query = n_query
+        self.query_emb_m = nn.Embedding(n_query, dq_dec)
+        self.dec_ca_layers = nn.ModuleList()
+        self.dec_sa_layers = nn.ModuleList()
+
+        n_head = dq_dec // d_head
+        for i in range(n_dec_layer):
+            dec_ca_layer = tsfm.AttnLayer(dq_dec, dk_dec, dv_dec, n_head)
+            dec_sa_layer = tsfm.AttnLayer(dq_dec, dk_dec, dv_dec, n_head)
+            self.dec_ca_layers.append(dec_ca_layer)
+            self.dec_sa_layers.append(dec_sa_layer)
+
+        # regression
+        self.ln = nn.LayerNorm(dv_dec)
+        self.box_reg = base.MLP(dv_dec, dv_dec * 2, 4, 2)
+        self.cls_reg = nn.Linear(dv_dec, n_cls, bias=False)
+
+    def forward(self, src, src_pos_emb):
+        src_with_pos = torch.concat([src, src_pos_emb], dim=-1)
+        query_indices = torch.arange(self.n_query, device=src.device).view(1, self.n_query).repeat(src.shape[0], 1)
+        q_dec = self.query_emb_m(query_indices)
+
+        for dec_ca, dec_sa in zip(self.dec_ca_layers, self.dec_sa_layers):
+            q_dec = dec_ca(q_dec, src_with_pos, src_with_pos, q_dec)
+            q_dec = dec_sa(q_dec, q_dec, q_dec, q_dec)
+
+        boxes = F.sigmoid(self.box_reg(self.ln(q_dec)))
+        cls_logits = self.cls_reg(self.ln(q_dec))
+
+        return boxes, cls_logits
+
+
+class DETR(nn.Module):
+    def __init__(self, d_cont, d_enc_coord_emb, d_head, n_enc_layer, n_dec_layer, n_query, exam_diff=True):
+        super().__init__()
+
+        self.encoder = enc.Encoder(n_enc_layer, d_cont, d_head, d_enc_coord_emb)
+        self.decoder = DetrDecoder(d_cont, d_enc_coord_emb, d_head, n_dec_layer, n_query)
+
+        self.exam_diff = exam_diff
+
+    def forward(self, x):
+
+        # encoder
+        src, pos_emb = self.encoder(x)
+
+        # regression
+        boxes, cls_logits = self.decoder(src, pos_emb)
+
+        if self.exam_diff and x.shape[0] > 1:
+            enc_diff = (src[0] - src[1]).abs().mean()
+            logits_diff = (cls_logits[0] - cls_logits[1]).abs().mean()
+        else:
+            enc_diff = 0
+            logits_diff = 0
+
+        return boxes, cls_logits, enc_diff, logits_diff
+
+
+if __name__ == '__main__':
+    img = torch.randn([4, 3, img_size[0], img_size[1]])
+    model = DETR(d_cont=256, d_pos=128, d_head=64, n_dec_layer=4, n_enc_layer=4, n_query=100)
+    print(model(img)[0].shape)
---
+++ od/train_base.py
@@ -0,0 +1,118 @@
+import os
+import torch
+from torch import optim
+from od import anno, detr_dataset, detr_model_base, match, eval
+from common.config import train_annotation_file, train_img_od_dict_file, img_size, \
+    train_base_bsz, model_save_dir, model_save_stride, device_type
+from od.config import loss_weights, n_query
+import focalloss
+import time
+import numpy as np
+from torchvision.ops import distance_box_iou_loss
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+device = torch.device(device_type)
+
+model = detr_model_base.DETR(d_cont=384, d_enc_coord_emb=64, d_head=64, n_enc_layer=20, n_dec_layer=8, n_query=n_query)
+model.to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=1e-5)
+
+dicts = anno.build_img_dict(train_annotation_file, train_img_od_dict_file, task='od')
+
+
+def train(epoch, batch_size, population, num_sample, weight_recover=0.5, gamma=4):
+    ds = detr_dataset.OdDataset(dicts, n_query, train=True, sample_num=population, random_shift=True)
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=False)
+    for i in range(epoch):
+        for j, (img, boxes_gt_xyxy, cids_gt, _, img_id) in enumerate(dl):
+            img = img.to(device)
+            cids_gt = cids_gt.to(torch.long)
+            cids_gt = cids_gt.to(device)
+            boxes_gt_xyxy = boxes_gt_xyxy.to(device) / max(img_size)
+            img = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(img)
+
+            B = img.shape[0]
+
+            boxes_pred_xyxy, cls_logits_pred, enc_diff, logits_diff = model(img)
+            gt_pos_mask = (cids_gt > 0)
+            gt_pos_mask = gt_pos_mask.view(B, 1, n_query) * 1
+
+            t = time.time()
+            _, cols = match.assign_query(boxes_gt_xyxy, boxes_pred_xyxy, cids_gt, cls_logits_pred, gt_pos_mask, anchors=None)
+            t_match = time.time() - t
+            cols = torch.tensor(np.stack(cols), device=device)
+
+            cls_pos_num = torch.sum(gt_pos_mask, dim=-1)
+
+            gt_matched_indices_batch = torch.arange(B, device=device).view(B, 1). \
+                expand(B, n_query).contiguous().view(B * n_query)
+
+            gt_matched_indices_query = cols.view(B * n_query)
+
+            query_pos_mask = (cols < cls_pos_num).view(B * n_query) * 1
+            n_pos = query_pos_mask.sum()
+
+            # cls loss
+            cids_gt = cids_gt[(gt_matched_indices_batch, gt_matched_indices_query)]
+            cls_logits_pred = cls_logits_pred.view(B * n_query, -1)
+
+            cls_pred = cls_logits_pred.argmax(dim=-1)
+
+            accu, recall, f1, n_tp = eval.eval_pred(cls_pred, cids_gt, query_pos_mask)
+
+            ce_alpha = torch.tensor(loss_weights, device=device) ** weight_recover
+            cls_loss = focalloss.focal_loss(cls_logits_pred, cids_gt, ce_alpha, gamma=gamma).mean()
+
+            # box loss
+            boxes_gt_xyxy = boxes_gt_xyxy[(gt_matched_indices_batch, gt_matched_indices_query)]
+            box_loss = distance_box_iou_loss(boxes_pred_xyxy.view(B * n_query, -1), boxes_gt_xyxy.view(B * n_query, -1))
+            box_loss = box_loss * query_pos_mask
+            box_loss = box_loss.sum() / (n_pos + 1e-5)
+
+            loss = cls_loss * 5 + box_loss
+            optimizer.zero_grad()
+            t = time.time()
+            loss.backward()
+            t_bp = time.time() - t
+            # nn.utils.clip_grad_value_(model.parameters(), 0.05)
+            optimizer.step()
+
+            print(f'smp {num_sample}|epoch {i + 1}/{epoch}|batch {j}|'
+                  f'cl {cls_loss.detach().item() * 1000:.3f}|'
+                  f'bl {box_loss.detach().item():.3f}|'
+                  f'ac {accu:.3f}|rc {recall:.3f}: {n_tp}/{n_pos}|'
+                  f'dif {enc_diff.item():.3f} {logits_diff.item():.3f}|'
+                  f'tmch {t_match:.3f}|tbp {t_bp:.3f}|'
+                  # f'match {matched_pos_gt}|'
+                  f'img {" ".join(img_id)}')
+
+
+if __name__ == '__main__':
+    model_files = os.listdir(model_save_dir)
+    model_files = [f for f in model_files if f.endswith('.pt') and f.startswith('od_base')]
+    if len(model_files) == 0:
+        model_path_old = None
+        latest_version = 0
+    else:
+        versions = [int(f.split('.')[0].split('_')[-1]) for f in model_files]
+        latest_version = max(versions)
+        model_path_old = f'{model_save_dir}/od_base_{latest_version}.pt'
+        saved_state = torch.load(model_path_old, map_location=device)
+        model.load_state_dict(saved_state)
+    for i in range(500):
+        n_smp = latest_version + 1 + i
+        ts = time.time()
+        train(1, batch_size=train_base_bsz, population=1000, num_sample=n_smp, weight_recover=0, gamma=4)
+        te = time.time()
+        print(f'----------------------used {te-ts:.3f} secs---------------------------')
+        # train(400, batch_size=2, population=2, num_sample=i, weight_recover=0, gamma=4)
+        # train(2, batch_size=2, population=2, num_sample=i, weight_recover=.5, gamma=2)
+        if n_smp % model_save_stride == 0:
+            model_path_new = f'{model_save_dir}/od_base_{n_smp}.pt'
+            torch.save(model.state_dict(), model_path_new)
+            if model_path_old is not None:
+                os.remove(model_path_old)
+            model_path_old = model_path_new
+        # break
